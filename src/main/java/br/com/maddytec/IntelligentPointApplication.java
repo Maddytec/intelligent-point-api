7fb8bdb3ed3ed2b70397a1cd85fcb29379bbbac5
Madson Silva <maddytec@gmail.com>
2018-11-19 11:05:20 -0200

    First commit of files of the project

---
+++ src/main/java/br/com/maddytec/IntelligentPointApplication.java
@@ -0,0 +1,12 @@
+package br.com.maddytec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentPointApplication.class, args);
+	}
+}
