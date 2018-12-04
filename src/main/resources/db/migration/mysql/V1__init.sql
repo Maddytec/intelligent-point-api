2090675a5180d25da7ae9adab5a1bac2e3b6b3d5
Madson Silva <maddytec@gmail.com>
2018-12-03 23:44:43 -0200

    Controller of version database MYSQL with Flyway

---
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 10.129.76.12
+-- Tempo de geração: 03/12/2018 às 15:18
+-- Versão do servidor: 5.6.26-log
+-- Versão do PHP: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de dados: `intelligent_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `employee`
+--
+
+CREATE TABLE IF NOT EXISTS `employee` (
+  `id` bigint(20) NOT NULL,
+  `count_hours_lunch` float DEFAULT NULL,
+  `count_hours_work_day` float DEFAULT NULL,
+  `date_create` datetime DEFAULT NULL,
+  `date_update` datetime DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `number_document_employee` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `profile` varchar(255) DEFAULT NULL,
+  `value_hour` decimal(19,2) DEFAULT NULL,
+  `employer_id` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `employer`
+--
+
+CREATE TABLE IF NOT EXISTS `employer` (
+  `id` bigint(20) NOT NULL,
+  `date_create` datetime DEFAULT NULL,
+  `date_update` datetime DEFAULT NULL,
+  `employer` varchar(255) DEFAULT NULL,
+  `number_document_employer` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `hibernate_sequence`
+--
+
+CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Fazendo dump de dados para tabela `hibernate_sequence`
+--
+
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+(1),
+(1),
+(1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `register`
+--
+
+CREATE TABLE IF NOT EXISTS `register` (
+  `id` bigint(20) NOT NULL,
+  `date` datetime DEFAULT NULL,
+  `date_create` datetime DEFAULT NULL,
+  `date_update` datetime DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `employee_id` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Índices de tabelas apagadas
+--
+
+--
+-- Índices de tabela `employee`
+--
+ALTER TABLE `employee`
+ ADD PRIMARY KEY (`id`), ADD KEY `FKrmknjkdeoo3molal1gxkt9dar` (`employer_id`);
+
+--
+-- Índices de tabela `employer`
+--
+ALTER TABLE `employer`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `register`
+--
+ALTER TABLE `register`
+ ADD PRIMARY KEY (`id`), ADD KEY `FK27yj934o5cr7royh64xm6pihr` (`employee_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
