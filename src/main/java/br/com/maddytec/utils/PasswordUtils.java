52ffcf215c7e13db6238d35c7bf1251c7ab522fb
Madson Silva <maddytec@gmail.com>
2018-12-07 07:37:00 -0200

    Added Cript password

---
+++ src/main/java/br/com/maddytec/utils/PasswordUtils.java
@@ -0,0 +1,23 @@
+package br.com.maddytec.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class PasswordUtils {
+	
+	private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+	
+	public static String generateBCrypt(String password) {
+		if(password == null) {
+			return password;
+		}
+		
+		log.info("Generate hash wich BCrypt.");
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder.encode(password);
+	}
+}
